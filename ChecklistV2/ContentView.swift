28d63e767b8f09fd1238e441320f8e9350b23fca
Do-Yeol Baek <dyb1129@gmail.com>
2021-05-25 14:54:09 +0900

    Initial Commit

---
+++ ChecklistV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV2
+//
+//  Created by ys on 2021/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
