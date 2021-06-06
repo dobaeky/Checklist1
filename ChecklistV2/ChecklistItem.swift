b3a2d0accf3813f4e71ebf983714a6172ff87bad
Do-Yeol Baek <dyb1129@gmail.com>
2021-06-06 18:15:39 +0900

    Cut and paste struct ChecklistItem into ChecklistItem.swift

---
+++ ChecklistV2/ChecklistItem.swift
@@ -0,0 +1,15 @@
+//
+//  ChecklistItem.swift
+//  ChecklistV2
+//
+//  Created by ys on 2021/06/06.
+//
+
+import Foundation
+
+
+struct ChecklistItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var isChecked: Bool = false
+}
